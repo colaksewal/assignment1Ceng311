e8ea9388f2329e951dbfa71f84165faa7ccedbde
colaksewal <108463004+colaksewal@users.noreply.github.com>
2024-03-15 22:44:02 +0300

    Update activity4.php

--- in-ClassActivity4/activity4.php
+++ in-ClassActivity4/activity4.php
@@ -1,43 +1,3 @@
-<?php
-// Function to get exchange rates
-function getExchangeRates() {
-    $exchangeRates = array(
-        "USD_EUR" => 0.85, // USD to EUR dönüşüm oranı
-        "EUR_USD" => 1.18, // EUR to USD dönüşüm oranı
-        "USD_CAD" => 1.25, // USD to CAD dönüşüm oranı
-        "CAD_USD" => 0.80, // CAD to USD dönüşüm oranı
-        "EUR_CAD" => 1.47, // EUR to CAD dönüşüm oranı
-        "CAD_EUR" => 0.68, // CAD to EUR dönüşüm oranı
-    );
-    return $exchangeRates;
-}
-
-// Function to calculate the converted amount
-function convertCurrency($amount, $fromCurrency, $toCurrency) {
-    $rates = getExchangeRates();
-    $exchangeKey = $fromCurrency . "_" . $toCurrency;
-    if (array_key_exists($exchangeKey, $rates)) {
-        $convertedAmount = $amount * $rates[$exchangeKey];
-        return number_format($convertedAmount, 2) . " " . $toCurrency; 
-    } else {
-        return "Dönüşüm oranı bulunamadı.";
-    }
-}
-
-// Form gönderildikten sonra girilen değeri alıp, tekrar forma yerleştir
-$fromAmount = isset($_GET["fromAmount"]) ? $_GET["fromAmount"] : "";
-$fromCurrency = isset($_GET["fromCurrency"]) ? $_GET["fromCurrency"] : "USD"; // Varsayılan olarak USD seçildi
-$toCurrency = isset($_GET["toCurrency"]) ? $_GET["toCurrency"] : "USD"; // Varsayılan olarak USD seçildi
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // Form gönderildiyse, dönüşüm işlemini gerçekleştir
-    $convertedAmount = convertCurrency($fromAmount, $fromCurrency, $toCurrency);
-} else {
-    // Form gönderilmediyse, varsayılan dönüştürülmüş miktarı ayarla
-    $convertedAmount = "";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,27 +7,27 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 <body>
     <form action="activity4.php" method="GET">
         <table>
-            <tr>
+        <tr>
                 <td>From:</td>
-                <td><input type="text" name="fromAmount" value="<?php echo $fromAmount; ?>"></td>
+                <td><input type="text" name="fromAmount" value="<?php if(isset($_GET['fromAmount'])) echo $_GET['fromAmount']; ?>"></td>
                 <td>Currency:</td>
                 <td>
                     <select name="fromCurrency">
-                        <option value="USD" <?php if($fromCurrency == "USD") echo "selected"; ?>>USD</option>
-                        <option value="CAD" <?php if($fromCurrency == "CAD") echo "selected"; ?>>CAD</option>
-                        <option value="EUR" <?php if($fromCurrency == "EUR") echo "selected"; ?>>EUR</option>
+                        <option value="USD" <?php if(isset($_GET['fromCurrency']) && $_GET['fromCurrency'] == 'USD') echo 'selected'; ?>>USD</option>
+                        <option value="CAD" <?php if(isset($_GET['fromCurrency']) && $_GET['fromCurrency'] == 'CAD') echo 'selected'; ?>>CAD</option>
+                        <option value="EUR" <?php if(isset($_GET['fromCurrency']) && $_GET['fromCurrency'] == 'EUR') echo 'selected'; ?>>EUR</option>
                     </select>
                 </td>
             </tr>
             <tr>
                 <td>To:</td>
-                <td><input type="text" name="toAmount" value="<?php echo $convertedAmount; ?>" readonly></td>
+                <td><input type="text" name="toAmount" value="<?php if(isset($_GET['toAmount'])) echo $_GET['toAmount']; ?>" readonly></td>
                 <td>Currency:</td>
                 <td>
                     <select name="toCurrency">
-                        <option value="USD" <?php if($toCurrency == "USD") echo "selected"; ?>>USD</option>
-                        <option value="CAD" <?php if($toCurrency == "CAD") echo "selected"; ?>>CAD</option>
-                        <option value="EUR" <?php if($toCurrency == "EUR") echo "selected"; ?>>EUR</option>
+                        <option value="USD" <?php if(isset($_GET['toCurrency']) && $_GET['toCurrency'] == 'USD') echo 'selected'; ?>>USD</option>
+                        <option value="CAD" <?php if(isset($_GET['toCurrency']) && $_GET['toCurrency'] == 'CAD') echo 'selected'; ?>>CAD</option>
+                        <option value="EUR" <?php if(isset($_GET['toCurrency']) && $_GET['toCurrency'] == 'EUR') echo 'selected'; ?>>EUR</option>
                     </select>
                 </td>
             </tr>
@@ -79,5 +39,38 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             </tr>
         </table>
     </form>
+
+    <?php
+    // get exchange rates
+    function getExchangeRates() {
+        $exchangeRates = array(
+            "USD_EUR" => 0.85, // USD to EUR dönüşüm oranı
+            "EUR_USD" => 1.18, // EUR to USD dönüşüm oranı
+            "USD_CAD" => 1.25, // USD to CAD dönüşüm oranı
+            "CAD_USD" => 0.80, // CAD to USD dönüşüm oranı
+            "EUR_CAD" => 1.47, // EUR to CAD dönüşüm oranı
+            "CAD_EUR" => 0.68, // CAD to EUR dönüşüm oranı
+        );
+        return $exchangeRates;
+    }
+
+    // Function to calculate the converted amount
+    function convertCurrency($amount, $fromCurrency, $toCurrency) {
+        $rates = getExchangeRates();
+        $convertedAmount = $amount * $rates[$fromCurrency . "_" . $toCurrency];
+        return $convertedAmount;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["fromAmount"]) && isset($_GET["fromCurrency"]) && isset($_GET["toCurrency"])) {
+        // Get form data
+        $fromAmount = $_GET["fromAmount"];
+        $fromCurrency = $_GET["fromCurrency"];
+        $toCurrency = $_GET["toCurrency"];
+        // Perform conversion
+        $convertedAmount = convertCurrency($fromAmount, $fromCurrency, $toCurrency);
+        // Display result in the input field
+        echo "<script>document.getElementsByName('toAmount')[0].value = '{$convertedAmount} {$toCurrency}';</script>";
+    }
+    ?>
 </body>
 </html>
